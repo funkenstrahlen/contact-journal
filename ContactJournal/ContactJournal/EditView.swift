420e1d061bc3fbc52f574b319e63fe02b0aa67fe
Stefan Trauth <mail@stefantrauth.de>
2020-10-14 15:42:42 +0200

    create preview for edit view

--- ContactJournal/ContactJournal/EditView.swift
+++ ContactJournal/ContactJournal/EditView.swift
@@ -27,3 +27,9 @@ struct EditView: View {
         })
     }
 }
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView(item: Item(context: PersistenceController.preview.container.viewContext)).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
