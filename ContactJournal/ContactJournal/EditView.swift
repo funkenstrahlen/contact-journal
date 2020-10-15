3696871afdbb5d5802c55adcc6961d486002d4c2
Stefan Trauth <mail@stefantrauth.de>
2020-10-15 20:42:16 +0200

    remove footer

--- ContactJournal/ContactJournal/EditView.swift
+++ ContactJournal/ContactJournal/EditView.swift
@@ -17,7 +17,7 @@ struct EditView: View {
             // check if item is valid because it might be deleted and this causes a crash here
             if(!item.isFault) {
                 DatePicker("Datum", selection: $item.timestamp, in: ...Date())
-                Section(header: Text("Kontakte"), footer: Text("Hier kannst du dokumentieren mit wem du Kontakt hattest. Kam es zu einer Situation, in der du keinen Abstand halten konntest oder viele Menschen zusammenkamen?")) {
+                Section(header: Text("Kontakte")) {
                     TextEditor(text: $item.content)
                 }
             }
