2218567bae394ed7969367f592a89b1e237ad6ac
Stefan Trauth <mail@stefantrauth.de>
2020-10-23 13:53:37 +0200

    live update when cloud data changes

--- ContactJournal/ContactJournal/Model/Persistence.swift
+++ ContactJournal/ContactJournal/Model/Persistence.swift
@@ -30,6 +30,7 @@ struct PersistenceController {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        container.viewContext.automaticallyMergesChangesFromParent = true
     }
     
     static func deleteDeprecatedItems() {
