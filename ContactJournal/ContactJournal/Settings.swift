656282cb86cfc29976aae6eda1a58cacb6cf69c3
Stefan Trauth <mail@stefantrauth.de>
2020-10-25 11:09:25 +0100

    move link to impressum into form

--- ContactJournal/ContactJournal/Settings.swift
+++ ContactJournal/ContactJournal/Settings.swift
@@ -13,29 +13,24 @@ struct Settings: View {
     @StateObject var userSettings = UserSettings()
     
     var body: some View {
-        ZStack {
-            Form {
-                Section(footer: Text("Für die Nachvollziehbarkeit von Infektionen sind alte Einträge nicht mehr relevant. Wenn diese Funktion aktiviert ist, dann werden diese Einträge automatisch aus deinem Kontakt-Tagebuch entfernt.")) {
-                    Toggle("Einträge älter als 14 Tage automatisch löschen", isOn: $shouldAutomaticallyDeleteDeprecatedItems)
-                }
-                Section(footer: Text("Erhalte einmal täglich eine Push Benachrichtigung, die dich daran erinnert dein Kontakt-Tagebuch zu pflegen.")) {
-                    Toggle("Erinnerung als Push Benachrichtigung", isOn: $userSettings.shouldSendPushNotification)
-                    if userSettings.shouldSendPushNotification {
-                        DatePicker("", selection: $userSettings.notificationTime, displayedComponents: .hourAndMinute)
-                            .datePickerStyle(WheelDatePickerStyle())
-                            .labelsHidden()
-                            .frame(maxHeight: 100)
-                            .clipped()
-                    }
-                }
-            }.navigationBarTitle("Einstellungen")
-            VStack {
-                Spacer()
-                Link("Impressum & Datenschutzerklärung", destination: URL(string: "https://stefantrauth.de/contact-journal-privacy-policy.html")!)
-                    .font(.footnote)
-                    .padding(.vertical)
+        Form {
+            Section(footer: Text("Für die Nachvollziehbarkeit von Infektionen sind alte Einträge nicht mehr relevant. Wenn diese Funktion aktiviert ist, dann werden diese Einträge automatisch aus deinem Kontakt-Tagebuch entfernt.")) {
+                Toggle("Einträge älter als 14 Tage automatisch löschen", isOn: $shouldAutomaticallyDeleteDeprecatedItems)
             }
-        }
+            Section(footer: Text("Erhalte einmal täglich eine Push Benachrichtigung, die dich daran erinnert dein Kontakt-Tagebuch zu pflegen.")) {
+                Toggle("Erinnerung als Push Benachrichtigung", isOn: $userSettings.shouldSendPushNotification)
+                if userSettings.shouldSendPushNotification {
+                    DatePicker("", selection: $userSettings.notificationTime, displayedComponents: .hourAndMinute)
+                        .datePickerStyle(WheelDatePickerStyle())
+                        .labelsHidden()
+                        .frame(maxHeight: 100)
+                        .clipped()
+                }
+            }
+            Section {
+                Link("Impressum & Datenschutzerklärung", destination: URL(string: "https://stefantrauth.de/contact-journal-privacy-policy.html")!)
+            }
+        }.navigationBarTitle("Einstellungen")
     }
 }
 
