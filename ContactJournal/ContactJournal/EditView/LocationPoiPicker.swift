63eb7bdeafee014152601e9095a390151812d8cd
Stefan Trauth <mail@stefantrauth.de>
2020-10-31 12:37:01 +0100

    improve mapkit row display

--- ContactJournal/ContactJournal/EditView/LocationPoiPicker.swift
+++ ContactJournal/ContactJournal/EditView/LocationPoiPicker.swift
@@ -8,18 +8,6 @@
 import SwiftUI
 import MapKit
 
-struct MapItemRow: View {
-    let item: MKMapItem
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
-            Text(item.name ?? "")
-            Text(item.placemark.postalAddress?.description ?? "")
-        }
-        
-    }
-}
-
 struct LocationPoiPicker: View {
     @State private var matchingItems = [MKMapItem]()
     
@@ -28,6 +16,41 @@ struct LocationPoiPicker: View {
             LocationSearchBar(matchingItems: $matchingItems, placeholder: "z.B. Starbucks Berlin")
             ForEach(matchingItems, id: \.self) { item in
                 MapItemRow(item: item)
+            }
+        }
+    }
+}
+
+struct MapItemRow: View {
+    let item: MKMapItem
+    
+    private var address: String {
+        var string = ""
+        if let street = item.placemark.postalAddress?.street, !street.isEmpty {
+            string.append("\(street), ")
+        }
+        string.append("\(item.placemark.postalAddress?.city ?? "")")
+        return string
+    }
+    
+    private var iconName: String {
+        switch item.pointOfInterestCategory {
+        default: return "mappin.circle.fill"
+        }
+    }
+    
+    private var iconColor: Color {
+        switch item.pointOfInterestCategory {
+        default: return Color.red
+        }
+    }
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            Image(systemName: iconName).foregroundColor(iconColor).font(.title2)
+            VStack(alignment: .leading, spacing: 5) {
+                Text(item.name ?? "").font(.headline)
+                Text(address).font(.subheadline).foregroundColor(.secondary)
             }
         }
     }
