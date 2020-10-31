4f624c1546facc943e24efdd4b5d4abb8b65db96
Stefan Trauth <mail@stefantrauth.de>
2020-10-31 12:16:04 +0100

    refactor

--- ContactJournal/ContactJournal/EditView/LocationPoiPicker.swift
+++ ContactJournal/ContactJournal/EditView/LocationPoiPicker.swift
@@ -25,80 +25,10 @@ struct LocationPoiPicker: View {
     
     var body: some View {
         List {
-            SearchBar(matchingItems: $matchingItems, placeholder: "z.B. Starbucks Berlin")
+            LocationSearchBar(matchingItems: $matchingItems, placeholder: "z.B. Starbucks Berlin")
             ForEach(matchingItems, id: \.self) { item in
                 MapItemRow(item: item)
             }
-        }
-    }
-}
-
-struct SearchBar: UIViewRepresentable {
-
-    @Binding var matchingItems: [MKMapItem]
-    
-    let placeholder: String
-
-    class Coordinator: NSObject, UISearchBarDelegate {
-        
-        private var search: MKLocalSearch?
-        private var searchText = ""
-        
-        @Binding var matchingItems: [MKMapItem]
-
-        init(matchingItems: Binding<[MKMapItem]>) {
-            _matchingItems = matchingItems
-        }
-
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            self.searchText = searchText
-            if searchText.isEmpty {
-                matchingItems = []
-            }
-        }
-        
-        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-            updateSearchResults()
-            searchBar.resignFirstResponder()
-        }
-        
-        func updateSearchResults() {
-            search?.cancel()
-            let request = MKLocalSearch.Request()
-            request.naturalLanguageQuery = searchText
-            request.resultTypes = [.address, .pointOfInterest]
-            request.pointOfInterestFilter = MKPointOfInterestFilter(excluding: [.atm, .fireStation, .parking])
-            search = MKLocalSearch(request: request)
-            
-            search?.start { response, _ in
-                DispatchQueue.main.async {
-                    self.matchingItems = response?.mapItems ?? []
-                }
-            }
-        }
-    }
-    func makeCoordinator() -> SearchBar.Coordinator {
-        return Coordinator(matchingItems: $matchingItems)
-    }
-
-    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
-        let searchBar = UISearchBar(frame: .zero)
-        searchBar.delegate = context.coordinator
-        searchBar.autocapitalizationType = .none
-        searchBar.searchBarStyle = .minimal
-        searchBar.placeholder = placeholder
-        return searchBar
-    }
-
-    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
-    }
-    
-}
-
-struct LocationPoiPicker_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            LocationPoiPicker()
         }
     }
 }
