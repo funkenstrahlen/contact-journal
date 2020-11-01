fbfbce757b1a2f578d42e62822a1ed3a4e4a770a
Stefan Trauth <mail@stefantrauth.de>
2020-11-01 11:52:59 +0100

    Revert "create delete item button"
    This reverts commit 659c4bf45236b0e4bd2b2af8cf05e8c4fc3f8145.

--- ContactJournal/ContactJournal/EditView/EditView.swift
+++ ContactJournal/ContactJournal/EditView/EditView.swift
@@ -28,7 +28,7 @@ struct EditView: View {
     @State private var showsLocationPicker = false
     @State private var riskLevel = RiskLevel.low
     
-    let deleteItem: (_ item: Item) -> Void
+    @Binding var isPresented: Bool
     
     var body: some View {
         // check if item is valid because it might be deleted and this causes a crash here
@@ -107,12 +107,6 @@ struct EditView: View {
                         LocationPoiPicker(selectedLocationAddress: $item.location, showsLocationPicker: $showsLocationPicker)
                     })
                 }
-                
-                Button(action: {
-                    deleteItem(item)
-                }) {
-                    Label("Eintrag löschen", systemImage: "trash")
-                }.foregroundColor(.red)
             }
             .navigationBarTitle(Text(navigationBarTitle), displayMode: .inline)
             .onDisappear(perform: {
