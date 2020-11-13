d66890a36e9abcd54be4220f1f3ff9a73e4d143b
Stefan Trauth <mail@stefantrauth.de>
2020-11-13 09:46:15 +0100

    remove donation view

--- ContactJournal/ContactJournal/AppDelegate.swift
+++ ContactJournal/ContactJournal/AppDelegate.swift
@@ -11,19 +11,6 @@ import StoreKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        SKPaymentQueue.default().add(self)
         return true
-    }
-}
-
-extension AppDelegate: SKPaymentTransactionObserver {
-    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-        for transaction in transactions {
-            switch transaction.transactionState {
-            case .purchasing, .deferred: break
-            case .failed, .purchased, .restored: queue.finishTransaction(transaction)
-            @unknown default: break
-            }
-        }
     }
 }
