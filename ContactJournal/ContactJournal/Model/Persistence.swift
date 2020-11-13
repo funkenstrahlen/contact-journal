d79f3faf1c4cd9ab418146f3532b94ad5d68fe80
Stefan Trauth <mail@stefantrauth.de>
2020-11-13 09:47:53 +0100

    remove cloudkit sync

--- ContactJournal/ContactJournal/Model/Persistence.swift
+++ ContactJournal/ContactJournal/Model/Persistence.swift
@@ -10,10 +10,10 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-    let container: NSPersistentCloudKitContainer
+    let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentCloudKitContainer(name: "ContactJournal")
+        container = NSPersistentContainer(name: "ContactJournal")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
